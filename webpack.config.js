44597a7ba7a1dfd2b155254c0891587bfb72aa8e
Igor Santos <falaigors@gmail.com>
2022-07-20 23:22:41 -0300

    test library

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+/* eslint-disable */
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: ['./src/lib/index.ts'],
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'index.js',
+      libraryTarget: 'commonjs'
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    externals: {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+      }
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          loader: 'ts-loader',
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          options: {
+            transpileOnly: false
+          }
+        },
+        {
+          test: /\.s?[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2
+              }
+            },
+            'postcss-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [new CleanWebpackPlugin(), new MiniCssExtractPlugin()],
+    devtool: isProduction
+      ? 'nosources-source-map'
+      : 'eval-cheap-module-source-map'
+  };
+};
